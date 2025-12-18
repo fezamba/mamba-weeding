d3b8cc4d65597eca47ba08bded2aef83b915510c
fezamba <fepetitt@gmail.com>
2025-12-18 08:08:03 -0300

    create guest dataseeder

---
+++ backend/src/main/java/com/br/mamba_wedding/config/GuestDataSeeder.java
@@ -0,0 +1,43 @@
+package com.br.mamba_wedding.config;
+
+import com.br.mamba_wedding.guests.domain.Guest;
+import com.br.mamba_wedding.guests.domain.GuestStatus;
+import com.br.mamba_wedding.guests.infrastructure.GuestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Profile("dev")
+public class GuestDataSeeder implements CommandLineRunner {
+
+    private final GuestRepository guestRepository;
+
+    @Override
+    public void run(String... args) {
+        if (guestRepository.count() > 0) {
+            return;
+        }
+
+        Guest g1 = Guest.builder()
+                .nomeCompleto("Fabiana Maia")
+                .codigoConvite("Fabi123")
+                .statusConvite(GuestStatus.PENDENTE)
+                .lado("FAMILIA")
+                .build();
+
+        Guest g2 = Guest.builder()
+                .nomeCompleto("Cecile Azambuja")
+                .codigoConvite("Ceci123")
+                .statusConvite(GuestStatus.PENDENTE)
+                .lado("FAMILIA")
+                .build();
+
+        guestRepository.save(g1);
+        guestRepository.save(g2);
+
+        System.out.println(">>> Convidados de teste inseridos com sucesso");
+    }
+}
