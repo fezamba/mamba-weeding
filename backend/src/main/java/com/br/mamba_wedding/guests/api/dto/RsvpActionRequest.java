03d07df07a4f7d3f613d8a577d9e8702de384961
Fernando de Azambuja Petit <fernando.petit@bndes.gov.br>
2025-12-30 15:17:03 -0300

    improve bean validation dto

--- backend/src/main/java/com/br/mamba_wedding/guests/api/dto/RsvpActionRequest.java
+++ backend/src/main/java/com/br/mamba_wedding/guests/api/dto/RsvpActionRequest.java
@@ -2,15 +2,18 @@ package com.br.mamba_wedding.guests.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
 
-public record RsvpActionRequest (
+public record RsvpActionRequest(
     @NotBlank
     @Size(min = 3, max = 32)
     String codigoConvite,
 
     @Size(max = 120)
+    @Email
     String email,
 
+    //FIXME: adicionar validação de telefone com @Pattern(regexp)
     @Size(max = 30)
     String telefone,
 
